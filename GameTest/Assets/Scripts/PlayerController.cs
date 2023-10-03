90a0d79262f47ebfd7b24ffd2eeff610d5128bf1
Kobe <kobemartinez98@yahoo.com>
2023-10-03 12:20:17 -0700

    got the player to attack but only when running

--- GameTest/Assets/Scripts/PlayerController.cs
+++ GameTest/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,10 @@ public class PlayerController : MonoBehaviour
     private CharacterController characterController;
 
     private bool isMoving;
+    private float timeSinceLastClick = 0f;
+    private float comboTimeThreshold = 0.5f; // Time window between clicks for combo attacks
+    private int comboCount = 0;
+
     public float speed;
     public float rotationSpeed;
     public float gravity = 9.81f;
@@ -20,18 +24,13 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        // Get input from the WASD keys or arrow keys.
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
         Vector3 gravityVector = Vector3.down * gravity * Time.deltaTime;
         characterController.Move(gravityVector);
 
-        // Calculate the movement vector.
-        Vector3 movement = new Vector3(horizontalInput, 0.0f, verticalInput);
-        movement.Normalize();
-
-        characterController.Move(movement * speed * Time.deltaTime);
+        Vector3 movement = new Vector3(horizontalInput, 0.0f, verticalInput).normalized;
 
         if (movement != Vector3.zero)
         {
@@ -39,13 +38,33 @@ public class PlayerController : MonoBehaviour
             transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
         }
 
-        // Check if the player is providing movement input.
+        characterController.Move(movement * speed * Time.deltaTime);
+
         isMoving = movement.magnitude > 0;
 
-        animator.SetFloat("Speed", speed);
+        animator.SetFloat("Speed", movement.magnitude); // Use magnitude for speed, not a fixed value
 
         animator.SetBool("IsRunning", isMoving);
-        
 
+        if (isMoving && Input.GetMouseButtonDown(0))
+        {
+            if (Time.time > timeSinceLastClick + comboTimeThreshold)
+            {
+                comboCount = 0;
+            }
+
+            comboCount++;
+            timeSinceLastClick = Time.time;
+
+            comboCount = Mathf.Clamp(comboCount, 1, 3);
+
+            string attackAnimation = "Attack" + comboCount;
+            animator.SetBool("IsAttacking", true);
+            animator.SetTrigger(attackAnimation);
+        }
+        else
+        {
+            animator.SetBool("IsAttacking", false);
+        }
     }
 }
